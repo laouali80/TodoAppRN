91c73f5c0bc6454e76c378af4d368664d3fa077d
Laouali <laoualibachir2000@gmail.com>
2024-11-16 03:50:45 +0100

    FIRST COMMIT

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+
+// To provide a view from the mobile bar
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
+  );
+}
---
+++ data/todos.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    title: "work out1 ",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "work out 2",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "work out 3",
+    completed: false,
+  },
+
+  {
+    id: 4,
+    title: "work out 4",
+    completed: false,
+  },
+  {
+    id: 5,
+    title: "work out 5",
+    completed: true,
+  },
+  {
+    id: 6,
+    title: "work out 6",
+    completed: false,
+  },
+  {
+    id: 7,
+    title: "work out 7",
+    completed: true,
+  },
+];
